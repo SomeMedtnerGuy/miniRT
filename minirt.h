86ea6060c604963551defcb915067a247009cfc0
Joao Pedro Monteiro Fonseca <joamonte@c2r6s3.42porto.com>
2024-09-02 20:46:22 +0100

    start rays and spheres

--- minirt.h
+++ minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ndo-vale <ndo-vale@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joamonte <joamonte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 20:35:42 by ndo-vale          #+#    #+#             */
-/*   Updated: 2024/08/31 16:29:38 by ndo-vale         ###   ########.fr       */
+/*   Updated: 2024/09/02 19:53:09 by joamonte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,18 @@
 # define CANVAS_HEIGHT 500
 
 # define WINDOW_TITLE "miniRT"
+
+typedef struct s_ray
+{
+	t_tup4	origin;
+	t_tup4	direction;
+}	t_ray;
+
+typedef struct s_sphere //Acrescentar Material
+{
+	t_tup4	center;
+	float	radius;
+}	t_sphere;
 
 typedef struct s_canvas
 {
@@ -52,6 +64,15 @@ unsigned char	float_to_shade(float color_strength);
 int				tuple_to_color(t_tup4 tup4);
 
 //TESTS.C
-void    run_tests(void);
+void	run_tests(void);
+
+//Rays
+t_ray	ray(t_tup4 origin, t_tup4 direction);
+
+//SPHERE
+void	sphere_testing();
+
+//MAIN
+void	put_pixel(t_canvas *img, int x, int y, int color);
 
 #endif
---
+++ src/sphere.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sphere.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joamonte <joamonte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 16:21:54 by joamonte          #+#    #+#             */
+/*   Updated: 2024/09/02 20:42:50 by joamonte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+t_sphere	sphere()
+{
+	t_sphere	s;
+
+	s.center = tup4(100, 100, 100, TPOINT);
+	s.radius = 10;
+	return	(s);
+}
+
+bool	intersect(t_sphere sphere, t_ray ray, float *t1, float *t2)
+{
+	t_tup4	sphere_to_ray;
+	float	a, b, c, discriminant;
+
+	sphere_to_ray = subtract_tup4(ray.origin, sphere.center);
+
+	a = dot(ray.direction, ray.direction);
+	printf("A: %f\n", a);
+
+	b = 2 * dot(ray.direction, sphere_to_ray);
+	printf("B: %f\n", b);
+
+	c = dot(sphere_to_ray, sphere_to_ray) - 1;
+	printf("C: %f\n", c);
+
+	discriminant = b * b - 4 * a * c;
+
+	if (discriminant < 0)
+		return (false);
+
+	printf("Discriminan: %f\n", discriminant);
+
+	*t1 = (-b - sqrt(discriminant)) / (2 * a);
+	*t2 = (-b + sqrt(discriminant)) / (2 * a);
+
+	printf("t1: %f\n", *t1);
+	printf("t2: %f\n", *t2);
+
+	return (true);
+}
+
+void	sphere_testing(/* t_root r */)
+{
+	t_ray		A;
+	t_sphere	S;
+	float	intersection[2];
+	t_tup4	int_points[2];
+	bool	check;
+
+	A.origin = tup4(100, 0, 100, TPOINT);
+	A.direction = tup4(0, 1, 0, TVECTOR);
+
+	S = sphere();
+
+	check = intersect(S, A, &intersection[0], &intersection[1]);
+
+	printf("Point 1 Value: %f\n", intersection[0]);
+	printf("Point 2 Value: %f\n", intersection[1]);
+
+
+	printf("Point Test: %f\n",(intersection[0] * A.direction.y));
+
+	int_points[0] = tup4((A.origin.x + intersection[0] * A.direction.x), (A.origin.y + intersection[0] * A.direction.y),
+	 (A.origin.z + intersection[0] * A.direction.z), TPOINT);
+
+	int_points[1] = tup4((A.origin.x + intersection[1] * A.direction.x), (A.origin.y + intersection[1] * A.direction.y),
+	 (A.origin.z + intersection[1] * A.direction.z), TPOINT);
+
+	printf("Point 1 X Coordinate : %f\n", int_points[0].x);
+	printf("Point 1 Y Coordinate : %f\n", int_points[0].y);
+	printf("Point 1 Z Coordinate : %f\n", int_points[0].z);
+
+	printf("Point 2 X Coordinate : %f\n", int_points[1].x);
+	printf("Point 2 Y Coordinate : %f\n", int_points[1].y);
+	printf("Point 2 Z Coordinate : %f\n", int_points[1].z);
+
+	printf("Your boolean variable is: %s\n", check ? "true" : "false");
+
+/* 	if(check)
+	{
+		printf("It intersects\n");
+
+		put_pixel(r.canvas, A.origin.x, A.origin.y, 0xffffff);
+		put_pixel(r.canvas, S.center.x, S.center.y, 0xff0000);
+		put_pixel(r.canvas, int_points[0].x, int_points[0].y, 0x00ff00);
+		put_pixel(r.canvas, int_points[1].x, int_points[1].y, 0x00ff00);
+	}
+	else
+		printf("It doesnt intersect\n"); */
+}
