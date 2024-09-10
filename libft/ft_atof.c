0785c41f6e2049fb9da6d5247c7cf8ef9f9e9ec5
Nuno Vale <ndo-vale@c2r6s3.42porto.com>
2024-09-10 14:56:14 +0100

    added atof, isstr_float and pow to libft

---
+++ libft/ft_atof.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atof.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndo-vale <ndo-vale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 12:10:03 by ndo-vale          #+#    #+#             */
+/*   Updated: 2024/09/10 14:54:19 by ndo-vale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "errno.h"
+
+bool	ft_isstr_float(char *str)
+{
+	if (!str)
+		return (false);
+	if (*str != '+' || *str != '-' || !ft_isdigit(*str))
+		return (false);
+	str++;
+	while (*str && *str != '.')
+	{
+		if (!ft_isdigit(*str))
+			return (false);
+		str++;
+	}
+	if (*str)
+		str++;
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (false);
+	}
+	return (true);
+}
+
+static int	is_neg(char **str)
+{
+	if (**str == '+')
+		*str += 1;
+	else if (**str == '-')
+	{
+		*str += 1;
+		return (-1);
+	}
+	return (1);
+}
+
+float	ft_atof(char *str)
+{
+	float	out;
+	int		ffactor;
+	float	decimal;
+	int		neg;
+
+	out = 0;
+	neg = is_neg(&str);
+	while (*str && *str != '.')
+	{
+		out = out * 10 + *str - '0';
+		str++;
+	}
+	if (!*str)
+		return (out * neg);
+	str++;
+	decimal = 0;
+	ffactor = 0;
+	while (*str)
+	{
+		decimal = decimal * 10 + *str - '0';
+		ffactor++;
+		str++;
+	}
+	return ((out + (decimal / ft_pow(10, ffactor))) * neg);
+}
