7009683403057167a33e1df1e6d73f78ad8b2c69
joao <jpmf2000@gmail.com>
2024-09-12 09:06:19 +0100

    changes to get_next_line

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -3,110 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ndo-vale <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: joamonte <joamonte@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 07:42:52 by ndo-vale          #+#    #+#             */
-/*   Updated: 2024/05/05 08:48:00 by ndo-vale         ###   ########.fr       */
+/*   Updated: 2024/09/12 09:00:10 by joamonte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		setup(char **str, size_t *starti, char **line, int fd);
-char	*check_create_buffer(char *str, size_t *starti, int fd);
-char	*update_line_and_buffer(char *line, char *str, size_t starti, int fd);
-char	*ft_strsjoin(char *s1, char *s2, size_t size);
-
-char	*get_next_line(int fd)
+static char	*get_line(char **buffer)
 {
-	static size_t	starti;
-	static char		*str;
-	char			*line;
-	int				i;
+	char	*line;
+	char	*keep;
+	size_t	til_null;
+	size_t	til_new;
 
-	if (!setup(&str, &starti, &line, fd))
-		return (str = NULL, NULL);
-	i = starti;
-	while (str[i++] != '\n')
+	til_new = strlen_at(*buffer, '\n');
+	if ((*buffer)[til_new] == '\n')
+		til_new++;
+	line = cpy_buffer(*buffer, til_new);
+	if (!line)
+		return (NULL);
+	til_null = strlen_at(*buffer, '\0');
+	keep = cpy_buffer(*buffer + til_new, til_null - til_new + 1);
+	if (!keep)
 	{
-		if (i >= BUFFER_SIZE)
-		{
-			line = update_line_and_buffer(line, str, starti, fd);
-			if (line == NULL)
-				return (str = NULL, NULL);
-			starti = 0;
-			i = 0;
-		}
+		free (line);
+		return (NULL);
 	}
-	line = ft_strsjoin(line, str + starti, i - starti);
-	if (line == NULL)
-		return (free(str), str = NULL, NULL);
-	return (starti = i, line);
-}
-
-char	*update_line_and_buffer(char *line, char *str, size_t starti, int fd)
-{
-	line = ft_strsjoin(line, str + starti, BUFFER_SIZE - starti);
-	if (line == NULL)
-		return (free(str), NULL);
-	ft_bzero(str, BUFFER_SIZE + 1);
-	if (read(fd, str, BUFFER_SIZE) < 1)
-		return (free(line), free(str), NULL);
+	free(*buffer);
+	*buffer = keep;
 	return (line);
 }
 
-int	setup(char **str, size_t *starti, char **line, int fd)
+static char	*get_current_buffer(int fd, char *buffer)
 {
-	*str = check_create_buffer(*str, starti, fd);
-	if (*str == NULL)
-		return (0);
-	*line = (char *)malloc(sizeof(char));
-	if (*line == NULL)
-		return (free(*str), 0);
-	(*line)[0] = 0;
-	return (1);
+	char	*current;
+	ssize_t	bytes;
+
+	bytes = 1;
+	current = (char *)malloc(BUFFER_SIZE + 1);
+	if (!current)
+		return (NULL);
+	while (bytes > 0 && !find_chr(buffer, '\n'))
+	{
+		bytes = read(fd, current, BUFFER_SIZE);
+		if (bytes == 0)
+			break ;
+		if (bytes == -1)
+		{
+			free(current);
+			return (NULL);
+		}
+		current[bytes] = '\0';
+		buffer = merge_previous_and_current(buffer, current);
+	}
+	free(current);
+	if (strlen_at(buffer, '\0') > 0)
+		return (buffer);
+	return (NULL);
 }
 
-char	*check_create_buffer(char *str, size_t *starti, int fd)
+char	*get_next_line(int fd)
 {
-	if (str != NULL)
-		return (str);
-	*starti = 0;
-	str = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	ft_bzero(str, BUFFER_SIZE + 1);
-	if (read(fd, str, BUFFER_SIZE) < 1)
-	{
-		free(str);
-		return (NULL);
-	}
-	return (str);
-}
+	static char	*buffer;
+	char		*line;
 
-char	*ft_strsjoin(char *s1, char *s2, size_t size)
-{
-	char	*final_str;
-	size_t	i;
-	size_t	j;
-
-	final_str = (char *)malloc((ft_strlen(s1) + size + 1) * sizeof(char));
-	if (final_str == NULL)
-		return (free(s1), NULL);
-	i = 0;
-	while (i < ft_strlen(s1))
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = get_current_buffer(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = get_line(&buffer);
+	if (!buffer[0])
 	{
-		final_str[i] = s1[i];
-		i++;
+		free (buffer);
+		buffer = NULL;
 	}
-	j = 0;
-	while (j < size)
-	{
-		final_str[i] = s2[j];
-		i++;
-		j++;
-	}
-	final_str[i] = '\0';
-	free(s1);
-	return (final_str);
+	return (line);
 }
